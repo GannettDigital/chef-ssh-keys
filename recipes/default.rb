0db917293dbc47cb91501b5813b189b9b6fef4c5
Eric Moritz <ericmoritz@gmail.com>
2015-10-15 19:41:38 -0400

    DL-60 fixed the spec issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,13 +2,10 @@ if node['ssh_keys']
   node['ssh_keys'].each do |node_user, bag_users|
     next unless node_user
     next unless bag_users
-    next unless node['etc']
-    next unless node['etc']['passwd']
-
+    next unless node['etc'] && node['etc']['passwd']
+    
     # Getting node user data
     user = node['etc']['passwd'][node_user]
-
-    next unless user
 
     # Defaults for new user
     user = { 'uid' => node_user, 'gid' => node_user, 'dir' => "/home/#{node_user}" } unless user
